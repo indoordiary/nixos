12bea1496f846976b0bad55cd8123a0ccf5fb71b
indoordiary <lfn2333333@gmail.com>
2025-04-11 05:05:36 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
-  description = "self-using configuration.";
+  description = "NixOS configuration";
 
   inputs = {
-
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,44 +13,43 @@
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, ... }:
-
-    (let
+  outputs = inputs@{ self, nixpkgs, home-manager, zen-browser, ... }:
+    let
       system = "x86_64-linux";
       genRev = {
         system.configurationRevision = self.rev or null;
-        system.nixos.label =
-          with builtins;
-            if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
-            then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
-            else "dirty";
+        system.nixos.label = with builtins;
+          if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
+          then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
+          else "dirty";
       };
-
     in {
       nixosConfigurations.ChenHsi-Desktop = nixpkgs.lib.nixosSystem {
-      
         inherit system;
-
+        specialArgs = { inherit inputs; };
+        
         modules = [
           ./configuration/system.nix
           ./configuration/user.nix
           ./configuration/programs.nix
           ./configuration/desktop/niri.nix
           ./configuration/device/RBP152022.nix
-
+          
           home-manager.nixosModules.home-manager
-     	    {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.chenhsi = import ./configuration/home.nix;
-            home-manager.backupFileExtension = "backup";
-           }
-
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.chenhsi = { pkgs, inputs, ... }: 
+                import ./configuration/home.nix { inherit inputs pkgs; };
+              backupFileExtension = "backup";
+            };
+          }
+          
           genRev
         ];
+      };
     };
-  });
 }
