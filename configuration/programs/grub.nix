bf268c9537bc1a8b946e0c45c32d18321f698a33
indoordiary <lfn2333333@gmail.com>
2025-04-11 04:07:40 +0800

    Update grub.nix

--- configuration/programs/grub.nix
+++ configuration/programs/grub.nix
@@ -7,8 +7,6 @@
         device = "nodev";
         efiSupport = true;
         useOSProber = true;
-        theme = pkgs.catppuccin-grub;
-        gfxmodeEfi = "1920x1080";
       };
       efi = {
         canTouchEfiVariables = true;
