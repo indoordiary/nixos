c445b6bcd95a5e6e94d18a8e64af894adc2ccf2c
indoordiary <lfn2333333@gmail.com>
2025-04-11 06:05:15 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "NixOS configuration";
+  description = "self-using configuration.";
 
   inputs = {
+
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,58 +16,41 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, zen-browser, ... }:
-    let
+  outputs = { self, nixpkgs, home-manager,  zen-browser, ... }:
+
+    (let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      
       genRev = {
         system.configurationRevision = self.rev or null;
-        system.nixos.label = with builtins;
-          if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
-          then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
-          else "dirty";
+        system.nixos.label =
+          with builtins;
+            if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
+            then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
+            else "dirty";
       };
+
     in {
-      nixosConfigurations.ChenHsi-Desktop = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.BrokenShine-Desktop = nixpkgs.lib.nixosSystem {
+      
         inherit system;
-        specialArgs = { 
-          inherit inputs;
-          lib = nixpkgs.lib;
-          config = { _module.args = { inherit pkgs; }; };  # 确保 config 可用
-        };
-        
+
         modules = [
           ./configuration/system.nix
           ./configuration/user.nix
           ./configuration/programs.nix
           ./configuration/desktop/niri.nix
           ./configuration/device/RBP152022.nix
-          
+
           home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.chenhsi = { config, pkgs, inputs, lib, ... }:  # 添加 config 参数
-                import ./configuration/home.nix { 
-                  inherit config inputs pkgs lib;  # 传递 config
-                };
-              backupFileExtension = "backup";
-            };
-          }
-          
+     	    {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.chenhsi = import ./configuration/home.nix;
+            home-manager.backupFileExtension = "backup";
+           }
+
           genRev
         ];
-      };
-      
-      homeConfigurations.chenhsi = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { 
-          inherit inputs;
-          config = { _module.args = { inherit pkgs; }; };  # 确保 config 可用
-        };
-        modules = [ ./configuration/home.nix ];
-      };
     };
+  });
 }
