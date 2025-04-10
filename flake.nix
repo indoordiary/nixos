cd36a114f4b6335ae42aad4d43e1ba33fce1104c
indoordiary <lfn2333333@gmail.com>
2025-04-11 07:08:17 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,36 +2,32 @@
   description = "self-using configuration.";
 
   inputs = {
-
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager,  zen-browser, ... }:
-
-    (let
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }:
+    let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
       genRev = {
         system.configurationRevision = self.rev or null;
-        system.nixos.label =
-          with builtins;
-            if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
-            then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
-            else "dirty";
+        system.nixos.label = with builtins;
+          if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
+          then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
+          else "dirty";
       };
-
     in {
       nixosConfigurations.ChenHsi-Desktop = nixpkgs.lib.nixosSystem {
-      
         inherit system;
 
         modules = [
@@ -41,22 +37,21 @@
           ./configuration/desktop/niri.nix
           ./configuration/device/RBP152022.nix
 
-          home-manager.nixosModules.home-manager
-     	    {
+          home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.chenhsi = ./configuration/home.nix;
             home-manager.backupFileExtension = "backup";
-           }
+            home-manager.users.chenhsi = ./configuration/home.nix;
+          }
 
           genRev
         ];
 
-        specialArgs = { 
+        specialArgs = {
           inputs = {
             zen-browser = zen-browser;
           };
         };
-     };
-  });
+      };
+    };
 }
