2423df36148bdb3a26e123117af66e5207fa4a62
indoordiary <lfn2333333@gmail.com>
2025-04-11 14:54:38 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,52 +3,65 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       
-      specialArgs = {
-        inherit inputs;
-        inherit (inputs) zen-browser;
-      };
-
       revisionInfo = {
-        system.configurationRevision = self.rev or "dirty";
-        system.nixos.label = 
-          if (self ? dirtyRev || !(self ? rev)) 
-          then "dirty" 
-          else builtins.substring 0 7 self.rev;
+        system.configurationRevision = self.rev or null;
+        system.nixos.label = with builtins;
+          if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
+          then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
+          else "dirty";
       };
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          nixpkgs-fmt
-        ];
-      };
-
       nixosConfigurations.ChenHsi-Desktop = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = specialArgs;
-        modules = [
+
+        modules = 
           ./configuration/system.nix
           ./configuration/user.nix
+          ./configuration/programs.nix
+          ./configuration/desktop/niri.nix
+          ./configuration/device/RBP152022.nix
+
           home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.chenhsi = import ./configuration/home.nix;
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "backup";
+              
+              users.chenhsi = {
+                imports =  ./configuration/home.nix ;
+              };
+              
+               关键修正：传递完整 inputs 对象
+              extraSpecialArgs = { 
+                inherit inputs; 
+                inherit (inputs) zen-browser; 
+              };
+            };
           }
+
           revisionInfo
-        ];
+        ;
+
+         系统层级的参数传递
+        specialArgs = { inherit inputs; };
       };
     };
-}
+}写法对吗？
