65310d7591099a4196a7fc742859b675b4022652
indoordiary <lfn2333333@gmail.com>
2025-04-11 13:59:42 +0800

    Update user.nix

--- configuration/user.nix
+++ configuration/user.nix
@@ -5,7 +5,7 @@
     mutableUsers = false;
     users.chenhsi = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "adbuser" "audio" "video" "input" "docker" "networkmanager" ];
+      extraGroups = [ "wheel" "adbuser" "uucp" "audio" "video" "libvirtd" "kvm" "input" "seat" ];
       useDefaultShell = true;
       hashedPassword = "$6$8/ZJ0jr/BWf4at92$PmRuEML2eiCHcRe/vuc54xgSkU.T7bO6ljUqczhkpw3kAv1mTl.PLMOhRuG.yIiEklJGwcHq3szit4PPkZaEM0";
     };
