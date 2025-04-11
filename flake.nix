2ea6531ba0244a9bec014a09ca0aae3d2390635a
indoordiary <lfn2333333@gmail.com>
2025-04-11 15:14:47 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,8 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs = { self, nixpkgs, home-manager, zen-browser, ... } @ inputs:
@@ -21,51 +13,42 @@
       pkgs = nixpkgs.legacyPackages.${system};
       
       revisionInfo = {
-        system.configurationRevision = self.rev or null;
-        system.nixos.label = with builtins;
-          if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
-          then "${substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
-          else "dirty";
+        system.configurationRevision = self.rev or "dirty";
+        system.nixos.label = 
+          if (self ? dirtyRev || !(self ? rev)) 
+          then "dirty"
+          else builtins.substring 0 7 self.rev;
       };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          nixpkgs-fmt
-        ];
+        packages = with pkgs; [ git nixpkgs-fmt ];
       };
+
       nixosConfigurations.ChenHsi-Desktop = nixpkgs.lib.nixosSystem {
         inherit system;
+        specialArgs = { inherit inputs zen-browser; };
 
-        modules = 
+        modules = [
           ./configuration/system.nix
           ./configuration/user.nix
           ./configuration/programs.nix
           ./configuration/desktop/niri.nix
           ./configuration/device/RBP152022.nix
 
-          home-manager.nixosModules.home-manager {
-            home-manager = {
+          ({ 
+            imports = [ home-manager.nixosModules.home-manager ];
+            config.home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               backupFileExtension = "backup";
-              
-              users.chenhsi = {
-                imports =  ./configuration/home.nix ;
-              };
-              
-              extraSpecialArgs = { 
-                inherit inputs; 
-                inherit (inputs) zen-browser; 
-              };
+              users.chenhsi.imports = [ ./configuration/home.nix ];
+              extraSpecialArgs = { inherit inputs zen-browser; };
             };
-          }
+          })
 
           revisionInfo
-        ;
-
-        specialArgs = { inherit inputs; };
+        ];
       };
     };
 }
