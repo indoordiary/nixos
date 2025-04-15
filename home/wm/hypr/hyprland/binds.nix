2749df76a0c49b5c829202f1f2be59362befee5e
indoordiary <lfn2333333@gmail.com>
2025-04-15 17:53:41 +0800

    Update binds.nix

--- home/wm/hypr/hyprland/binds.nix
+++ home/wm/hypr/hyprland/binds.nix
@@ -105,8 +105,8 @@ lib.mkIf config.optional.hypr.enable {
 
       bindl = [
         # media controls
-        "switch:on:Lid Switch,exec,hyprctl keyword monitor "eDP-1, 3200X2000@90, 0x0, 2"
-        "switch:off:Lid Switch,exec,hyprctl keyword monitor "eDP-1, disable"
+        "switch:on:Lid Switch,exec,hyprctl keyword monitor 'eDP-1, 3200X2000@90, 0x0, 2'"
+        "switch:off:Lid Switch,exec,hyprctl keyword monitor 'eDP-1, disable'"
       ];
 
       bindle = [
