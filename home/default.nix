ce26db3307e7020bb39624abcee86fa1d9d59ce0
indoordiary <lfn2333333@gmail.com>
2025-04-16 10:18:58 +0800

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -17,8 +17,10 @@
       BROWSER = "${config.profile.browser}";
       TERMINAL = "${config.profile.terminal}";
 
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       QT_SCALE_FACTOR = "2";
+      GDK_SCALE ="2";
+      GDK_DPL_SCALE = "0.5";
 
     };
 
