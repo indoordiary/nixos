2ef9de9fa91bc19196af3feb8c4a3186e3c1d0d3
indoordiary <lfn2333333@gmail.com>
2025-04-16 08:53:48 +0800

    Update default.nix

--- home/wm/hypr/hyprland/default.nix
+++ home/wm/hypr/hyprland/default.nix
@@ -1,23 +1,34 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   cursor = "Bibata-Modern-Classic-Hyprcursor";
   cursorPackage = pkgs.bibata-hyprcursor;
 in
 {
-  imports = [ ./binds.nix ./rules.nix ./settings.nix ];
+  imports = [
 
+    ./binds.nix
+    ./rules.nix
+    ./settings.nix
+  ];
   config = lib.mkIf config.optional.hypr.enable {
     home.packages = with pkgs; [
       networkmanagerapplet
       qt6.qtwayland
       libsForQt5.qt5.qtwayland
+
+      # screenshot
       grimblast
     ];
 
-    # 设置光标主题
     xdg.dataFile."icons/${cursor}".source = "${cursorPackage}/share/icons/${cursor}";
 
-    # 系统托盘配置
+    # fake a tray to let apps start
+    # https://github.com/nix-community/home-manager/issues/2064
     systemd.user.targets.tray = {
       Unit = {
         Description = "Home Manager System Tray";
@@ -25,33 +36,21 @@ in
       };
     };
 
-    # 环境变量（关键修改！）
     home.sessionVariables = {
-      GDK_SCALE = "2";               # GTK 全局缩放
-      GDK_DPI_SCALE = "0.5";          # 防止 GTK 字体过小
-      QT_SCALE_FACTOR = "2";          # QT 全局缩放
-      QT_ENABLE_HIDPI= "1"; # QT 自动适配屏幕缩放
-      XCURSOR_SIZE = "32";            # 光标大小
+      # XDG_CURRENT_DESKTOP = "Hyprland";
+      # XDG_SESSION_DESKTOP = "Hyprland";
+      GDK_SCALE = "2";
+      GDK_DPL_SCALE = "0.5";
+
       QT_QPA_PLATFORM = "wayland";
       SDL_VIDEODRIVER = "wayland";
       XDG_SESSION_TYPE = "wayland";
-    };
-
-    # Hyprland 主配置（关键修改！）
-    wayland.windowManager.hyprland = {
-      enable = true;
-      systemd.enable = true;
-      settings = {
-        env = [
-          "GDK_BACKEND,wayland,x11"
-        ];
-        exec-once = [
-          "hyprctl setcursor ${cursor} 32"
-          "xprop -root -f XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set XWAYLAND_GLOBAL_OUTPUT_SCALE 2" 
-        ];
-      };
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     };
 
     catppuccin.hyprland.enable = true;
+    wayland.windowManager.hyprland = {
+      enable = true;
+    };
   };
 }
